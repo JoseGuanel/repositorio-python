08017bb4e320faad128d9d2ca89b41cce8b5226d
josé guanel <141767185+JoseGuanel@users.noreply.github.com>
2024-05-27 12:07:01 -0400

    Create variables.py

---
+++ Introducción a python(apuntes)/variables.py
@@ -0,0 +1,16 @@
+#listas
+list1= ["Python", "Java", "C++"] 
+print(list1)
+
+list1.append("Rubi")
+print(list1)
+
+list1.remove("Java")
+print(list1)
+
+listnum= list(range(1,11)) #lista con numeros del 1 al 10
+print(listnum)
+
+#Listas pueden tener diferentes tipos de variables
+listrandom= [2100, "Numeros", True, False, 2.102, (2,10)]
+print(listrandom)
