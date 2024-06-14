c3245f345425baccfb733a0fc4cc87b941633eb3
josé guanel <141767185+JoseGuanel@users.noreply.github.com>
2024-06-14 09:59:49 -0400

    actualizacion de la materia

---
+++ Introducción a python(apuntes)/condicionales.py
@@ -0,0 +1,14 @@
+### IF ELSE
+edad=69
+if edad >=18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
+
+### elif 
+if edad >= 18 and edad < 65:
+    print("Eres mayor de edad.")
+elif edad >= 65:
+    print("Eres adulto mayor.")
+else:
+    print("Eres menor de edad.")
